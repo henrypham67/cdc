2407161b514ccfcce4732535190186d8183d0339
Henry Pham <minhhieu060799@gmail.com>
2025-02-01 15:55:29 +0700

    add opensearch, prometheus

--- infra/helm_releases.tf
+++ infra/helm_releases.tf
@@ -1,19 +1,38 @@
 resource "helm_release" "strimzi-kafka-operator" {
-  depends_on = [ module.eks ]
-  
-  name       = "strimzi-kafka-operator"
-  repository = "https://strimzi.io/charts/"
-  chart      = "strimzi-kafka-operator"
-  namespace  = "kafka"
+  depends_on = [module.eks]
+
+  name             = "strimzi-kafka-operator"
+  repository       = "https://strimzi.io/charts/"
+  chart            = "strimzi-kafka-operator"
+  namespace        = "kafka"
   create_namespace = true
 }
 
-resource "helm_release" "strimzi-kafka-operator" {
-  depends_on = [ module.eks ]
-  
-  name       = "strimzi-kafka-operator"
-  repository = "https://strimzi.io/charts/"
-  chart      = "strimzi-kafka-operator"
-  namespace  = "kafka"
+resource "helm_release" "opensearch" {
+  depends_on = [module.eks]
+
+  name             = "opensearch"
+  repository       = "https://opensearch-project.github.io/helm-charts/"
+  chart            = "opensearch"
+  namespace        = "opensearch"
   create_namespace = true
+
+  values = [
+    file("${path.module}/values/opensearch.yaml")
+  ]
+}
+
+resource "helm_release" "kube-prometheus-stack" {
+  depends_on = [module.eks]
+
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "68.3.3"
+  namespace        = "observability"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values/kube-prometheus-stack.yaml")
+  ]
 }
