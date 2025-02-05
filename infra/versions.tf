530dae93599108eb15d04edfc1047880441baf31
Henry Pham <minhhieu060799@gmail.com>
2025-02-05 17:51:54 +0700

    add kubectl version

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,13 +2,14 @@ terraform {
   required_version = ">= 1.10"
 
   required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.35.1"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.17.0"
+    }
+
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "2.1.3"
     }
   }
 }
