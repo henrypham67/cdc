9feb92235fd4e850401d0600e117f5fec7fd9c51
Henry Pham <minhhieu060799@gmail.com>
2025-02-05 17:51:26 +0700

    add mysql rds

--- infra/rds.tf
+++ infra/rds.tf
@@ -13,43 +13,38 @@ locals {
   }
 }
 
-
-
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
-module "db" {
+# PostgreSQL RDS Instance
+module "postgres_db" {
   source = "terraform-aws-modules/rds/aws"
 
-  identifier = local.name
+  identifier            = "${local.name}-postgres"
+  engine                = "postgres"
+  engine_version        = "16"
+  instance_class        = "db.t4g.medium"
+  family                = "postgres16"
+  allocated_storage     = 10
+  max_allocated_storage = 20
 
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine                   = "postgres"
-  engine_version           = "16"
-  engine_lifecycle_support = "open-source-rds-extended-support-disabled"
-  instance_class           = "db.t4g.medium"
-  family                   = "postgres16"
-
-  allocated_storage     = 20
-  max_allocated_storage = 50
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "postgres"
+  db_name  = local.db_name
   username = "postgres"
-  password = "nZh2mXMD9uRrXfJctV71" # for POC only
   port     = 5432
+  # for POC only
+  manage_master_user_password = false
+  password                    = var.db_password
 
   db_subnet_group_name   = module.vpc.database_subnet_group
   vpc_security_group_ids = [module.security_group.security_group_id]
 
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
   create_cloudwatch_log_group     = true
 
-  backup_retention_period = 1
+  backup_retention_period = 0
   skip_final_snapshot     = true
   deletion_protection     = false
 
@@ -58,31 +53,72 @@ module "db" {
   parameters = [
     {
       name  = "autovacuum"
-      value = 1
+      value = "1"
     },
     {
       name  = "client_encoding"
       value = "utf8"
     },
     {
-      name  = "rds.logical_replication"
-      value = "1"
+      name         = "rds.logical_replication"
+      value        = "1"
       apply_method = "pending-reboot"
     }
   ]
 
   tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
-  cloudwatch_log_group_tags = {
-    "Sensitive" = "high"
-  }
 }
 
-output "db_instance_address" {
-  value = module.db.db_instance_address
+# MySQL RDS Instance
+module "mysql_db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "${local.name}-mysql"
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+  engine               = "mysql"
+  engine_version       = "8.0"
+  family               = "mysql8.0" # DB parameter group
+  major_engine_version = "8.0"      # DB option group
+  instance_class       = "db.t4g.medium"
+
+  allocated_storage     = 10
+  max_allocated_storage = 20
+
+  db_name  = local.db_name
+  username = "debezium"
+  port     = 3306
+  # for POC only
+  manage_master_user_password = false
+  password                    = var.db_password
+
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  backup_retention_period = 0
+
+  parameters = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    }
+  ]
+
+  tags = local.tags
+}
+
+# Outputs for PostgreSQL and MySQL
+output "postgres_db_instance_address" {
+  value = module.postgres_db.db_instance_address
+}
+
+output "mysql_db_instance_address" {
+  value = module.mysql_db.db_instance_address
 }
