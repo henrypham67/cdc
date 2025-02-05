c62cc166dee6564f20844acf7b1fa142e1f46e95
Henry Pham <minhhieu060799@gmail.com>
2025-02-05 17:51:05 +0700

    kubenetes -> kubectl provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,13 +1,8 @@
-# provider "kubectl" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.this.token
-#   load_config_file       = false
-# }
-
-provider "kubernetes" {
+provider "kubectl" {
+  apply_retry_count      = 15
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", local.name]
